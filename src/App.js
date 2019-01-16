13be580e1c936790b9512d48bb187ce5081ac5d6
James Priest <james-priest@outlook.com>
2019-01-16 05:03:52 -0800

    feat: Add Exercise info

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src="logo.svg" className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
+          <p>Exercise 2 - Passing Data</p>
         </header>
         <main className="App-main">
           <h2>How Popular is Your Favorite Movie?</h2>
