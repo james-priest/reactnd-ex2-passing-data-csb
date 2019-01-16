1274d378017d27de8b1a2d631b1dc1930dc949d9
James Priest <james-priest@outlook.com>
2019-01-16 02:50:58 -0800

    feat: Filter users by favoriteMovieID;  log output

--- src/UserList.js
+++ src/UserList.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class UserList extends Component {
   render() {
     const { movieID, profiles, users } = this.props;
-    console.log(profiles);
+    const filteredProfiles = profiles.filter(
+      profile => Number(profile.favoriteMovieID) === movieID
+    );
+    console.log(filteredProfiles);
     return <div>names...</div>;
   }
 }
